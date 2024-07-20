7e77e864b65d38a384f56da7427ab1733573693f
fatihkurtl <kurtf061@gmail.com>
2024-07-20 04:19:40 +0300

    general fix & seo settings

--- app.vue
+++ app.vue
@@ -1,33 +1,10 @@
 <script lang="ts" setup>
 import Navbar from '~/layouts/Navbar.vue'
 import Footer from '~/layouts/Footer.vue'
-
-const {
-  cookiesEnabled,
-  cookiesEnabledIds,
-  isConsentGiven,
-  isModalActive,
-  moduleOptions,
-} = useCookieControl()
-
-watch(
-  () => cookiesEnabledIds.value,
-  (current, previous) => {
-    if (
-      !previous?.includes('google-analytics') &&
-      current?.includes('google-analytics')
-    ) {
-      // cookie with id `google-analytics` got added
-      window.location.reload() // placeholder for your custom change handler
-    }
-  },
-  { deep: true },
-)
 </script>
 
 <template>
   <section class="bg-gray-800">
-    <CookieControl locale="en" />
     <NuxtLayout>
       <Navbar />
       <NuxtPage />
